73f4ff344649cef4704cd189d9fd4fb1fb80da5c
Sara Skardelly <85453371+saraskardelly@users.noreply.github.com>
2024-10-08 21:15:39 +0200

    Update Scatterplot_NDVIvs.LST.py

--- Scatterplot_NDVIvs.LST.py
+++ Scatterplot_NDVIvs.LST.py
@@ -1,23 +1,25 @@
+# In the following I created a scatterplot, to check for the relationship between Land Surface Temperature (LST) and the Normalized Difference Vegetation Index (NDVI)
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import linregress
 
-dataset_LST = r"C:\Users\skard\OneDrive\Desktop\LST.csv"
-dataset_NDVI = r"C:\Users\skard\OneDrive\Desktop\NDVI.csv"
-
+# here, upload the datasets for NDVI and LST
+dataset_LST =
+dataset_NDVI = 
 LST_data = pd.read_csv(dataset_LST)
 NDVI_data = pd.read_csv(dataset_NDVI)
-
 #print(LST_data.head())
 #print(NDVI_data.head())
 
+# this is not really necessary, but it is easier to work with it later, as some start and end dates differ each year
 LST_data['endDate'] = pd.to_datetime(LST_data['endDate'])
 NDVI_data['endDate'] = pd.to_datetime(NDVI_data['endDate'])
-
 LST_data_year_filtered = LST_data[LST_data['endDate'].dt.year == 2020]
 NDVI_data_year_filtered = NDVI_data[NDVI_data['endDate'].dt.year == 2020]
 
+# the common identifiactor is the ZBEZ code in both datasets
 LST_data_year_filtered = LST_data_year_filtered.sort_values(by='ZBEZ').reset_index(drop=True)
 NDVI_data_year_filtered = NDVI_data_year_filtered.sort_values(by='ZBEZ').reset_index(drop=True)
 
